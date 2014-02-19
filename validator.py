1e284a726a42b12fcea7facd443e4cda235ba196
Wiktor Wolak <wwolak@wwolak.pl>
2014-02-19 21:19:32 +0100

    Add ArgumentParser

--- validator.py
+++ validator.py
@@ -21,6 +21,8 @@ import argparse
 
 import numpy as np
 
+import base_parser
+
 
 def get_data_from_file(file_path):
     """
@@ -28,7 +30,13 @@ def get_data_from_file(file_path):
     In case of other data AssertionError will be raise.  
     """
     data = np.loadtxt(file_path)
-    assert len(data.shape) == 2 and data.shape[1] == 3
+    if len(data.shape) == 1 and data.shape[0] == 3:
+        data = data[np.newaxis,:]
+    if len(data.shape) != 2:
+        raise ValueError("Wrong format of data")
+    if data.shape[1] != 3:
+        raise ValueError("Wrong format of data."
+            "Got %d value per line, 3 expected" % data.shape[1])
     return data
     
 
@@ -53,5 +61,30 @@ def get_number_of_records(data):
     return len(data)
 
 if __name__ == '__main__':
-    pass #TODO: argparse
+    parser = base_parser.BaseParser(
+        description="""
+            Filtrate data.  Each line of data is valid only when value from 3.
+            column is equal to 1.
+            """,
+        epilog="") 
+    parser.add_argument('-l',
+        dest='line',
+        action='store_true',
+        help='Write number of filtrated data except just data.')
+    args = parser.parse_args()
+    data = get_data_from_file(args.input_file)
+    if args.filter_ == 'all':
+        pass
+    elif args.filter_ == 'valid':
+        data = get_valid_data(data)
+    elif args.filter_ == 'invalid':
+        data = get_invalid_data(data)
+    else:
+        raise Exception("Wrong argument parsing")
+    if args.line:
+        args.output_file.write('%d\n' % len(data))
+    else:
+        np.savetxt(args.output_file, data)
+
+    
 
